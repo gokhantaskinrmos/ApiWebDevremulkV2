e610451ef67a01713062aabb952c7a753a63f6ae
gokhantaskinrmos <gokhantaskin6061@gmail.com>
2023-02-08 10:51:06 +0300

    8Şubat_gokhan

--- DevreMulk/Controllers/Default.cs
+++ DevreMulk/Controllers/Default.cs
@@ -30,7 +30,7 @@ namespace DevreMulk.Controllers
         }
 
         [HttpPost]//Hatalık Toplam Çağrı Sayısı
-        [Route("tuğbatipinde")]
+        [Route("yarraktipinde")]
         public IActionResult GunlukKullaniciSkoru(string bas,string bit)
         {
             using var c = new Context();//
